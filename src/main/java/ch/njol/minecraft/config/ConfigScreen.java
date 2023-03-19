663688917598f51c3525f9e7ab5ff82a5946ea64
Peter Güttinger <njol@gmx.ch>
2023-03-19 12:43:50 +0100

    Subcategory support for 1.18

--- src/main/java/ch/njol/minecraft/config/ConfigScreen.java
+++ src/main/java/ch/njol/minecraft/config/ConfigScreen.java
@@ -1,10 +1,16 @@
 package ch.njol.minecraft.config;
 
 import ch.njol.minecraft.config.annotations.Category;
+import ch.njol.minecraft.config.annotations.Dropdown;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.function.Supplier;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.gui.entries.SubCategoryListEntry;
+import me.shedaniel.clothconfig2.impl.ConfigEntryBuilderImpl;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.TranslatableText;
 
@@ -18,6 +24,8 @@ public class ConfigScreen<T extends Options> extends Screen {
 	private final Supplier<T> optionsSupplier;
 	private final T defaultOptions;
 
+	private final HashMap<Subcategory, SubCategoryListEntry> subCategories = new HashMap<>();
+
 	protected ConfigScreen(Screen parent, String translateRoot, Supplier<T> optionsSupplier, T defaultOptions) {
 		super(new TranslatableText(translateRoot + ".title"));
 		this.parent = parent;
@@ -26,6 +34,7 @@ public class ConfigScreen<T extends Options> extends Screen {
 		this.defaultOptions = defaultOptions;
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	protected void init() {
 		ConfigBuilder config = ConfigBuilder.create()
@@ -36,10 +45,26 @@ public class ConfigScreen<T extends Options> extends Screen {
 
 		for (Field field : options.getClass().getDeclaredFields()) {
 			Category categoryAnnotation = field.getAnnotation(Category.class);
+			Dropdown dropdownAnnotation = field.getAnnotation(Dropdown.class);
 			if (categoryAnnotation == null || !options.categoryVisible(categoryAnnotation.value())) {
 				continue;
 			}
 			ConfigCategory category = config.getOrCreateCategory(new TranslatableText(translateRoot + ".category." + categoryAnnotation.value()));
+
+			if (dropdownAnnotation != null) {
+				Subcategory subcategory = new Subcategory(dropdownAnnotation.value(), categoryAnnotation);
+
+				if (subCategories.containsKey(subcategory)) {
+					subCategories.get(subcategory).getValue().add(ClothConfigSetup.buildConfigEntry(options, defaultOptions, field, translateRoot + ".option"));
+				} else {
+					ArrayList<AbstractConfigListEntry> list = new ArrayList<>();
+					list.add(ClothConfigSetup.buildConfigEntry(options, defaultOptions, field, translateRoot + ".option"));
+					SubCategoryListEntry entry = ConfigEntryBuilderImpl.create().startSubCategory(new TranslatableText(translateRoot + ".subcategory." + dropdownAnnotation.value()), list).build();
+					subCategories.put(subcategory, entry);
+					category.addEntry(entry);
+				}
+				continue;
+			}
 			category.addEntry(ClothConfigSetup.buildConfigEntry(options, defaultOptions, field, translateRoot + ".option"));
 		}
 
@@ -55,6 +80,9 @@ public class ConfigScreen<T extends Options> extends Screen {
 		if (client != null) {
 			client.setScreen(parent);
 		}
+	}
+
+	public record Subcategory(String name, Category category) {
 	}
 
 }
